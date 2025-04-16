413b815935dee1bf3aeb47782807dd5a43c3db4b
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-16 18:05:30 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,31 @@
-resource "aws_instance" "my-aws_instance" {
+resource "aws_instance" "main" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp2"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update -y
+              sudo apt-get install -y docker-ce
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo docker run -d -p 8080:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HTTPS=false openproject/openproject:15
+              EOF
+  tags = {
+    Name = "main-instance"
+  }
+}resource "aws_instance" "my-aws_instance" {
   ami = var.ami_value
   instance_type = var.instance_type
   subnet_id = var.subnet_id
