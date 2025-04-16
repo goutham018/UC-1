9049ad469479320cb41e37aa5eedec0102b8649b
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-16 18:04:07 +0530

    Create main.tf

---
+++ modules/security_group/main.tf
@@ -0,0 +1,60 @@
+ variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main-sg"
+  }
+}
+
+ resource "aws_security_group" "alb_sg" {
+   vpc_id = var.vpc_id
+
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+}
+
+
+
+output "web_security_group_id" {
+  value = aws_security_group.main.id
+}
+
+output "lb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
